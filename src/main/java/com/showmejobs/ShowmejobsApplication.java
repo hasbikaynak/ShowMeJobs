2052278010fb0285171e67c6ed7a59848b444e7c
kaynak <kaynakhasbi1@gmail.com>
2023-06-24 12:28:02 +0200

    project initial files

---
+++ src/main/java/com/showmejobs/ShowmejobsApplication.java
@@ -0,0 +1,13 @@
+package com.showmejobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowmejobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowmejobsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/showmejobs/ShowmejobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showmejobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowmejobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
